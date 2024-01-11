0225822576e60ad0a4859f93d8a2c06807ec3dd3
Divyansh Agarwal <divyansh.agarwal900@gmail.com>
2024-01-11 18:41:26 +0530

    commit frontend

--- src/pages/Profile/EditProfile/EditProfile.js
+++ src/pages/Profile/EditProfile/EditProfile.js
@@ -57,6 +57,7 @@ function EditChild({dob , setDob}){
               onChange={e => setDob(e.target.value)}
             />
             <Button className='e-button' onClick={() => { setOpen(false); }}>Cancel</Button>
+            <Button >Save</Button>
           </div>
         </Box>
       </Modal>
